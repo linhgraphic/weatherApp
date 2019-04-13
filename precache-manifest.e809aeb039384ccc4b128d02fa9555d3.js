184e04efd50b54e5b190e20081b6b9a954c96fcb
Linh <linhgraphic@gmail.com>
2019-04-13 17:19:38 +0100

    Updates

---
+++ precache-manifest.e809aeb039384ccc4b128d02fa9555d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "87c501c13f8a58b7ec66",
+    "url": "/static/css/main.91a11a62.chunk.css"
+  },
+  {
+    "revision": "87c501c13f8a58b7ec66",
+    "url": "/static/js/main.9ccfac8c.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "58311d38038931edb30d",
+    "url": "/static/js/2.3f7cb3d7.chunk.js"
+  },
+  {
+    "revision": "3d86f5ba5d1e9d9db203967693221e25",
+    "url": "/index.html"
+  }
+];
